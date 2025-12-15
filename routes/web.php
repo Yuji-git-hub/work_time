5d93021a14d2e6ebb017e7f6572a54662c3178fb
Yuji <ss.yuji1012@gmail.com>
2025-12-15 13:14:15 +0900

    ルーティング作成、readmeファイルの修正

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
+use App\Http\Controllers\SubjectController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('/subjects')->group(function () {
+    Route::get('/create', [SubjectController::class, 'create'])->name('subjects.create');
+    Route::get('', [SubjectController::class, 'index'])->name('subjects.index');
+    Route::post('', [SubjectController::class, 'store'])->name('subjects.store');
+    Route::get('/{subject}', [SubjectController::class, 'show'])->name(('subjects.show'));
+    Route::get('/{subject}/edit', [SubjectController::class, 'edit'])->name('subjects.edit');
+    Route::put('/{subject}', [SubjectController::class, 'update'])->name('subjects.update');
+    Route::delete('/{subject}', [SubjectController::class, 'destroy'])->name('subjects.destroy');
+});
+
+
