1f0f270a3ba2e6869d8df67811e6187326b192b1
Yuji <ss.yuji1012@gmail.com>
2025-12-18 19:19:31 +0900

    breeze導入

--- routes/web.php
+++ routes/web.php
@@ -7,14 +7,20 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::prefix('/subjects')->group(function () {
-    Route::get('/create', [SubjectController::class, 'create'])->name('subjects.create');
-    Route::get('', [SubjectController::class, 'index'])->name('subjects.index');
-    Route::post('', [SubjectController::class, 'store'])->name('subjects.store');
-    Route::get('/{subject}', [SubjectController::class, 'show'])->name(('subjects.show'));
-    Route::get('/{subject}/edit', [SubjectController::class, 'edit'])->name('subjects.edit');
-    Route::put('/{subject}', [SubjectController::class, 'update'])->name('subjects.update');
-    Route::delete('/{subject}', [SubjectController::class, 'destroy'])->name('subjects.destroy');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::prefix('/subjects')->group(function () {
+        Route::get('/create', [SubjectController::class, 'create'])->name('subjects.create');
+        Route::get('', [SubjectController::class, 'index'])->name('subjects.index');
+        Route::post('', [SubjectController::class, 'store'])->name('subjects.store');
+        Route::get('/{subject}', [SubjectController::class, 'show'])->name('subjects.show');
+        Route::get('/{subject}/edit', [SubjectController::class, 'edit'])->name('subjects.edit');
+        Route::put('/{subject}', [SubjectController::class, 'update'])->name('subjects.update');
+        Route::delete('/{subject}', [SubjectController::class, 'destroy'])->name('subjects.destroy');
+    });
 });
 
-
+require __DIR__.'/auth.php';
